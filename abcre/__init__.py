dae513401697bd9b1d000af6171cd7dd70158b60
Pat Laub <patrick.laub@gmail.com>
2021-05-01 15:47:08 +1000

    Remove old hilbertcurve local reference

--- abcre/__init__.py
+++ abcre/__init__.py
@@ -4,4 +4,3 @@ from .simulate import simulate_claim_data, simulate_claim_sizes
 from .smc import smc, compute_psi, Model, Psi, Fit
 from .wasserstein import wass_sumstats, wass_dist, identity, wrap_ss_curve_matching, wass_2Ddist, wass_2Ddist_ss
 from .weighted import resample, resample_and_kde, weighted_distplot
-from .hilbertcurve import HilbertCurve
