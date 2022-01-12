9772fcf7c9b28a625cab509518e86c89d6060c1d
Patrick Laub <patrick.laub@gmail.com>
2022-01-12 16:51:14 +1100

    Add test for the new 'SimulationModel'

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -49,3 +49,38 @@ def test_partially_observed_model():
     epsMin = 3
     fit = abc.smc(numItersData, popSize, xData, model, epsMin=epsMin, verbose=True)
     assert np.max(fit.dists) < epsMin
+
+
+def simulator(rg, T, theta):
+    lam = theta[0]
+    thetaSev = theta[1:]
+    freqs = rg.poisson(lam, size=T)
+
+    scale, cor = thetaSev
+
+    aggClaims = np.empty(T, np.float64)
+    for t in range(T):
+        R = freqs[t]
+        claims = scale * np.exp(cor * R) * rg.exponential(size=R)
+        aggClaims[t] = np.sum(claims)
+
+    return aggClaims
+
+
+def test_full_model_using_custom_simulator():
+    # This should give the exact same fit as in test_full_model.
+
+    # Specify model to fit
+    params = ("λ", "β", "δ")
+    prior = abc.IndependentUniformPrior([(0, 10), (0, 20), (-1, 1)], params)
+    model = abc.SimulationModel(simulator, prior)
+
+    epsMin = 6
+    fit = abc.smc(numIters, popSize, xData, model, epsMin=epsMin, verbose=True)
+    assert np.max(fit.dists) < epsMin
+
+
+if __name__ == "__main__":
+    test_full_model()
+    test_full_model_using_custom_simulator()
+    test_partially_observed_model()
