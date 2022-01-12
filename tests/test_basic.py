de03d20733e3a85ffd649d6625e1b5dbc82d0183
Patrick Laub <patrick.laub@gmail.com>
2022-01-12 16:25:40 +1100

    Assert that epsilon decreases in test_basic.py

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,15 +1,16 @@
+import numpy as np
 import numpy.random as rnd
 
 import approxbayescomp as abc
 
 numIters = 5
 numItersData = 10
-popSize = 500
-epsMin = 1
-
+popSize = 100
 
 # Frequency-Loss Model
-λ, β, δ = 4, 2, 0.2
+λ = 4
+β = 2
+δ = 0.2
 θ_True = λ, β, δ
 
 sev = "frequency dependent exponential"
@@ -17,29 +18,34 @@ freq = "poisson"
 psi = abc.Psi("sum")  # Aggregation process
 
 # Simulate some data to fit
-sample_sizes = [50, 250]
-T = sample_sizes[-1]
+T = 50
 
 rg = rnd.default_rng(123)
 freqs, sevs = abc.simulate_claim_data(rg, T, freq, sev, θ_True)
 xData = abc.compute_psi(freqs, sevs, psi)
 
-# Specify model to fit
-params = ("λ", "β", "δ")
-prior = abc.IndependentUniformPrior([(0, 10), (0, 20), (-1, 1)], params)
-model = abc.Model("poisson", "frequency dependent exponential", psi, prior)
 
-abcFits = []
+def test_simulation_size():
+    assert len(xData) == T
 
-for ss in sample_sizes:
-    fit = abc.smc(numIters, popSize, xData[:ss], model, epsMin=epsMin, verbose=True)
-    abcFits.append(fit)
 
-params = ("β", "δ")
-prior = abc.IndependentUniformPrior([(0, 20), (-1, 1)], params)
+def test_full_model():
+    # Specify model to fit
+    params = ("λ", "β", "δ")
+    prior = abc.IndependentUniformPrior([(0, 10), (0, 20), (-1, 1)], params)
+    model = abc.Model("poisson", "frequency dependent exponential", psi, prior)
 
-for ss in sample_sizes:
-    nDataSS = freqs[:ss]
-    model = abc.Model(nDataSS, "frequency dependent exponential", psi, prior)
-    fit = abc.smc(numItersData, popSize, xData[:ss], model, epsMin=epsMin, verbose=True)
-    abcFits.append(fit)
+    epsMin = 6
+    fit = abc.smc(numIters, popSize, xData, model, epsMin=epsMin, verbose=True)
+    assert np.max(fit.dists) < epsMin
+
+
+def test_partially_observed_model():
+    # Try fitting the same model but with the frequencies observed
+    params = ("β", "δ")
+    prior = abc.IndependentUniformPrior([(0, 20), (-1, 1)], params)
+    model = abc.Model(freqs, "frequency dependent exponential", psi, prior)
+
+    epsMin = 3
+    fit = abc.smc(numItersData, popSize, xData, model, epsMin=epsMin, verbose=True)
+    assert np.max(fit.dists) < epsMin
