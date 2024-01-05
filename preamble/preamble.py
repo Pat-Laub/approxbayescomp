07843fe5bda68175641e7f9db4d3685115656af1
Patrick Laub <patrick.laub@gmail.com>
2024-01-05 10:16:15 +1000

    Removed everything with mike 2.0.0

--- preamble/preamble.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-
-plt.rcParams["figure.figsize"] = (5.0, 2.0)
-plt.rcParams["figure.dpi"] = 350
-plt.rcParams["savefig.bbox"] = "tight"
-plt.rcParams["font.family"] = "serif"
-plt.rcParams["pgf.texsystem"] = "pdflatex"
