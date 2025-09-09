2010b97beff4982ef099dfade8b9c15a11bbbd39
Patrick Laub <patrick.laub@gmail.com>
2025-09-09 18:49:09 +1000

    Deployed 2743d07 to 0.2 with MkDocs 1.6.1 and mike 2.1.3

---
+++ 0.2/assets/setup-mathjax.js
@@ -0,0 +1,20 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [["\\(", "\\)"], ["$", "$"]],
+    displayMath: [["\\[", "\\]"], ["$$", "$$"]],
+    processEscapes: true,
+    processEnvironments: true,
+    packages: {'[+]': ['ams']}
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
+};
+
+document$.subscribe(() => {
+  MathJax.startup.defaultReady();
+  MathJax.startup.document.state(0);
+  MathJax.texReset();
+  MathJax.typesetPromise();
+});
